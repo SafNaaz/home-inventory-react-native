e5fae790ebbe3598b6c7cdae7130de6df38ad482
Safnas <iamsafnas@mgmail.com>
2026-02-15 21:33:26 +0530

    version update

--- src/screens/SettingsScreen.tsx
+++ src/screens/SettingsScreen.tsx
@@ -479,7 +479,7 @@ const SettingsScreen: React.FC = () => {
         
         <List.Item
           title="Household Inventory"
-          description="Version 7.1.0"
+          description="Version 7.1.1"
           left={() => <Icon name="information-outline" size={24} color={theme.colors.onSurface} />}
         />
         
