a352f310873f1827d9baaf25c2da2c3ac2219792
Safnas <iamsafnas@mgmail.com>
2026-02-21 19:01:46 +0530

    version

--- src/screens/SettingsScreen.tsx
+++ src/screens/SettingsScreen.tsx
@@ -490,7 +490,7 @@ const SettingsScreen: React.FC = () => {
         
         <List.Item
           title="HiHome"
-          description="Version 9.0.0"
+          description="Version 10.0.0"
           left={() => <Icon name="information-outline" size={24} color={theme.colors.onSurface} />}
         />
         
